1d9f4319a221072fd74b2a3cd17378fdb2c3c60e
H-Robert1998 <robertdelta1998@gmail.com>
2026-06-04 11:36:48 +0300

    Start production creating folders

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div className='maincontainer'>
+
+            </div>
+        </>
+    )
+}
+
+export default App
+// import "./SignInBtn.css"
+
+// function SignInBtn (){
+
+//     return(
+//         <div className="signInBtn_container">
+//             <div className="signInBtn_blok">Sign In</div>
+//         </div>
+//     );
+// }
+// export default SignInBtn
---
+++ src/page/HomePage.jsx
@@ -0,0 +1,11 @@
+// import "./HomePage.css"
+
+// function HomePage (){
+
+//     return(
+//         <div className="homePage_container">
+//             HomePage
+//         </div>
+//     );
+// }
+// export default HomePage
